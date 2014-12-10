04089d4390f6be4e13c6e13a30ea57677874c123
dandavis <rndme@users.noreply.github.com>
2014-12-10 04:20:49 -0600

    update regeisterElement polyfill, remove group support, minor cleanup

--- veneer.js
+++ veneer.js
@@ -6,10 +6,11 @@ What is does: allows custom HTML tags names, syncs those tag's properties and at
 */
 
 // (C) WebReflection document.registerElement() Polyfill  
-// Mit Style License. from https://github.com/WebReflection/document-register-element   
-if(!document.registerElement){
-(function(e,t,n,r){"use strict";function R(e,t){for(var n=0,r=e.length;n<r;n++)Q(e[n],t)}function U(e){for(var t=0,n=e.length,r;t<n;t++)r=e[t],K(r,c[X(r)])}function z(){B=0,R(t.querySelectorAll(h),"attached")}function W(e){return function(t){g.call(L,t)&&(Q(t,e),R(t.querySelectorAll(h),e))}}function X(e){var t=e.getAttribute("is");return d.call(l,t?t.toUpperCase():e.nodeName)}function V(e){var t=e.currentTarget,n=e.attrChange,r=e.prevValue,i=e.newValue;t.attributeChangedCallback&&t.attributeChangedCallback(e.attrName,n===e.ADDITION?null:r,n===e.REMOVAL?null:i)}function $(e){var t=W(e);return function(e){t(e.target)}}function J(e,t){var n=this;O.call(n,e,t),j.call(n,{target:n})}function K(e,t){N(e,t),q?q.observe(e,_):(H&&(e.setAttribute=J,e[i]=I(e),e.addEventListener(u,j)),e.addEventListener(o,V)),e.createdCallback&&(e.created=!0,e.createdCallback(),e.created=!1)}function Q(e,t){var n,r=X(e),i="attached",s="detached";-1<r&&(C(e,c[r]),r=0,t===i&&!e[i]?(e[s]=!1,e[i]=!0,r=1):t===s&&!e[s]&&(e[i]=!1,e[s]=!0,r=1),r&&(n=e[t+"Callback"])&&n.call(e))}if(r in t)return;var i="__"+r+(Math.random()*1e5>>0),s="extends",o="DOMAttrModified",u="DOMSubtreeModified",a=/^[A-Z][A-Z0-9]*(?:-[A-Z0-9]+)+$/,f=["ANNOTATION-XML","COLOR-PROFILE","FONT-FACE","FONT-FACE-SRC","FONT-FACE-URI","FONT-FACE-FORMAT","FONT-FACE-NAME","MISSING-GLYPH"],l=[],c=[],h="",p=t.documentElement,d=l.indexOf||function(e){for(var t=this.length;t--&&this[t]!==e;);return t},v=n.prototype,m=v.hasOwnProperty,g=v.isPrototypeOf,y=n.defineProperty,b=n.getOwnPropertyDescriptor,w=n.getOwnPropertyNames,E=n.getPrototypeOf,S=n.setPrototypeOf,x=!!n.__proto__,T=n.create||function G(e){return e?(G.prototype=e,new G):this},N=S||(x?function(e,t){return e.__proto__=t,e}:b?function(){function e(e,t){for(var n,r=w(t),i=0,s=r.length;i<s;i++)n=r[i],m.call(e,n)||y(e,n,b(t,n))}return function(t,n){do e(t,n);while(n=E(n));return t}}():function(e,t){for(var n in t)e[n]=t[n];return e}),C=S||x?function(e,t){g.call(t,e)||K(e,t)}:function(e,t){e[i]||(e[i]=n(!0),K(e,t))},k=e.MutationObserver||e.WebKitMutationObserver,L=(e.HTMLElement||e.Element||e.Node).prototype,A=L.cloneNode,O=L.setAttribute,M=t.createElement,_=k&&{attributes:!0,characterData:!0,attributeOldValue:!0},D=k||function(e){H=!1,p.removeEventListener(o,D)},P=!1,H=!0,B=0,j,F,I,q;k||(p.addEventListener(o,D),p.setAttribute(i,1),p.removeAttribute(i),H&&(j=function(e){var t=this,n,r,s;if(t===e.target){n=t[i],t[i]=r=I(t);for(s in r){if(!(s in n))return F(0,t,s,n[s],r[s],"ADDITION");if(r[s]!==n[s])return F(1,t,s,n[s],r[s],"MODIFICATION")}for(s in n)if(!(s in r))return F(2,t,s,n[s],r[s],"REMOVAL")}},F=function(e,t,n,r,i,s){var o={attrChange:e,currentTarget:t,attrName:n,prevValue:r,newValue:i};o[s]=e,V(o)},I=function(e){for(var t,n={},r=e.attributes,i=0,s=r.length;i<s;i++)t=r[i],n[t.name]=t.value;return n})),t[r]=function(n,r){y=n.toUpperCase(),P||(P=!0,k?(q=function(e,t){function n(e,t){for(var n=0,r=e.length;n<r;t(e[n++]));}return new k(function(r){for(var i,s,o=0,u=r.length;o<u;o++)i=r[o],i.type==="childList"?(n(i.addedNodes,e),n(i.removedNodes,t)):(s=i.target,s.attributeChangedCallback&&s.attributeChangedCallback(i.attributeName,i.oldValue,s.getAttribute(i.attributeName)))})}(W("attached"),W("detached")),q.observe(t,{childList:!0,subtree:!0})):(t.addEventListener("DOMNodeInserted",$("attached")),t.addEventListener("DOMNodeRemoved",$("detached"))),t.createElement=function(e,n){var r,i=M.apply(t,arguments);return n&&i.setAttribute("is",e=n.toLowerCase()),r=d.call(l,e.toUpperCase()),-1<r&&K(i,c[r]),i},L.cloneNode=function(e){var t=A.call(this,!!e),n=X(t);return-1<n&&K(t,c[n]),e&&U(t.querySelectorAll(h)),t});if(-1<d.call(l,y))throw new Error("A "+n+" type is already registered");if(!a.test(y)||-1<d.call(f,y))throw new Error("The type "+n+" is invalid");var i=function(){return t.createElement(p,u&&y)},o=r||v,u=m.call(o,s),p=u?r[s]:y,g=l.push(y)-1,y;return h=h.concat(h.length?",":"",u?p+'[is="'+n.toLowerCase()+'"]':p),i.prototype=c[g]=m.call(o,"prototype")?o.prototype:T(L),clearTimeout(B),B=setTimeout(z),i}})(window,document,Object,"registerElement");
-}
+// from:https://github.com/WebReflection/document-register-element fetched 2014-12-10
+/*! (C) WebReflection Mit Style License */
+(function(e,t,n,r){"use strict";function z(e,t){for(var n=0,r=e.length;n<r;n++)Y(e[n],t)}function W(e){for(var t=0,n=e.length,r;t<n;t++)r=e[t],U(r,p[V(r)])}function X(e){return function(t){b.call(A,t)&&(Y(t,e),z(t.querySelectorAll(d),e))}}function V(e){var t=e.getAttribute("is"),n=e.nodeName,r=m.call(h,t?f+t.toUpperCase():a+n);return t&&-1<r&&!$(n,t)?-1:r}function $(e,t){return-1<d.indexOf(e+'[is="'+t+'"]')}function J(e){var t=e.currentTarget,n=e.attrChange,r=e.prevValue,i=e.newValue;t.attributeChangedCallback&&e.attrName!=="style"&&t.attributeChangedCallback(e.attrName,n===e.ADDITION?null:r,n===e.REMOVAL?null:i)}function K(e){var t=X(e);return function(e){t(e.target)}}function Q(e,t){var n=this;M.call(n,e,t),j.call(n,{target:n})}function G(e,t){k(e,t),q?q.observe(e,D):(B&&(e.setAttribute=Q,e[i]=I(e),e.addEventListener(u,j)),e.addEventListener(o,J)),e.createdCallback&&(e.created=!0,e.createdCallback(),e.created=!1)}function Y(e,t){var n,r=V(e),i="attached",s="detached";-1<r&&(R(e,p[r]),r=0,t===i&&!e[i]?(e[s]=!1,e[i]=!0,r=1):t===s&&!e[s]&&(e[i]=!1,e[s]=!0,r=1),r&&(n=e[t+"Callback"])&&n.call(e))}if(r in t)return;var i="__"+r+(Math.random()*1e5>>0),s="extends",o="DOMAttrModified",u="DOMSubtreeModified",a="<",f="=",l=/^[A-Z][A-Z0-9]*(?:-[A-Z0-9]+)+$/,c=["ANNOTATION-XML","COLOR-PROFILE","FONT-FACE","FONT-FACE-SRC","FONT-FACE-URI","FONT-FACE-FORMAT","FONT-FACE-NAME","MISSING-GLYPH"],h=[],p=[],d="",v=t.documentElement,m=h.indexOf||function(e){for(var t=this.length;t--&&this[t]!==e;);return t},g=n.prototype,y=g.hasOwnProperty,b=g.isPrototypeOf,w=n.defineProperty,E=n.getOwnPropertyDescriptor,S=n.getOwnPropertyNames,x=n.getPrototypeOf,T=n.setPrototypeOf,N=!!n.__proto__,C=n.create||function Z(e){return e?(Z.prototype=e,new Z):this},k=T||(N?function(e,t){return e.__proto__=t,e}:S&&E?function(){function e(e,t){for(var n,r=S(t),i=0,s=r.length;i<s;i++)n=r[i],y.call(e,n)||w(e,n,E(t,n))}return function(t,n){do e(t,n);while(n=x(n));return t}}():function(e,t){for(var n in t)e[n]=t[n];return e}),L=e.MutationObserver||e.WebKitMutationObserver,A=(e.HTMLElement||e.Element||e.Node).prototype,O=A.cloneNode,M=A.setAttribute,_=t.createElement,D=L&&{attributes:!0,characterData:!0,attributeOldValue:!0},P=L||function(e){B=!1,v.removeEventListener(o,P)},H=!1,B=!0,j,F,I,q,R,U;T||N?(R=function(e,t){b.call(t,e)||G(e,t)},U=G):(R=function(e,t){e[i]||(e[i]=n(!0),G(e,t))},U=R),L||(v.addEventListener(o,P),v.setAttribute(i,1),v.removeAttribute(i),B&&(j=function(e){var t=this,n,r,s;if(t===e.target){n=t[i],t[i]=r=I(t);for(s in r){if(!(s in n))return F(0,t,s,n[s],r[s],"ADDITION");if(r[s]!==n[s])return F(1,t,s,n[s],r[s],"MODIFICATION")}for(s in n)if(!(s in r))return F(2,t,s,n[s],r[s],"REMOVAL")}},F=function(e,t,n,r,i,s){var o={attrChange:e,currentTarget:t,attrName:n,prevValue:r,newValue:i};o[s]=e,J(o)},I=function(e){for(var t,n,r={},i=e.attributes,s=0,o=i.length;s<o;s++)t=i[s],n=t.name,n!=="setAttribute"&&(r[n]=t.value);return r})),t[r]=function(n,r){w=n.toUpperCase(),H||(H=!0,L?(q=function(e,t){function n(e,t){for(var n=0,r=e.length;n<r;t(e[n++]));}return new L(function(r){for(var i,s,o=0,u=r.length;o<u;o++)i=r[o],i.type==="childList"?(n(i.addedNodes,e),n(i.removedNodes,t)):(s=i.target,s.attributeChangedCallback&&i.attributeName!=="style"&&s.attributeChangedCallback(i.attributeName,i.oldValue,s.getAttribute(i.attributeName)))})}(X("attached"),X("detached")),q.observe(t,{childList:!0,subtree:!0})):(t.addEventListener("DOMNodeInserted",K("attached")),t.addEventListener("DOMNodeRemoved",K("detached"))),t.addEventListener("readystatechange",function(e){z(t.querySelectorAll(d),"attached")}),t.createElement=function(e,n){var r=_.apply(t,arguments),i=m.call(h,(n?f:a)+(n||e).toUpperCase()),s=-1<i;return n&&(r.setAttribute("is",n=n.toLowerCase()),s&&(s=$(e.toUpperCase(),n))),s&&U(r,p[i]),r},A.cloneNode=function(e){var t=O.call(this,!!e),n=V(t);return-1<n&&U(t,p[n]),e&&W(t.querySelectorAll(d)),t});if(-2<m.call(h,f+w)+m.call(h,a+w))throw new Error("A "+n+" type is already registered");if(!l.test(w)||-1<m.call(c,w))throw new Error("The type "+n+" is invalid");var i=function(){return t.createElement(v,u&&w)},o=r||g,u=y.call(o,s),v=u?r[s].toUpperCase():w,b=h.push((u?f:a)+w)-1,w;return d=d.concat(d.length?",":"",u?v+'[is="'+n.toLowerCase()+'"]':v),i.prototype=p[b]=y.call(o,"prototype")?o.prototype:C(A),z(t.querySelectorAll(d),"attached"),i}})(window,document,Object,"registerElement");
+ 
+
  
  /* create a style tag to hold CSS for all the custom components */
  (function(x,d){d=document, x=d.getElementsByTagName("script")[0]; if(!x){return;}
@@ -137,13 +138,6 @@ veneer.elm=function elm(tagName, attribs, content){
 	return elm;
 };
 
-//get a groupg of related elements by group attrib
-veneer.group=function group(strGroup){
-	if(strGroup.group){strGroup=strGroup.group;}
-	return veneer.$( strGroup ? ("[group~='"+strGroup+"']") : "[group]")
-	
-};
-
 //a simple async url fetch:
 veneer.ajax=function aGet(strURL, fnCallBack) {
 	var e = new XMLHttpRequest;
@@ -164,13 +158,11 @@ veneer.importHTML=function(url){
 	return veneer.ajax(url, function(s){hd.innerHTML+=s;});
 };
 
-//elminates ~50% of common callbacks to fetch data:
+//eliminates ~50% of common callbacks to fetch data:
 veneer.assign=function assign(obj, key){ return function(v){ob[key]=v;};};
 	
-veneer.observe=function(obj, prop, callBack){
-	
-	var val=obj[prop];
-	
+veneer.observe=function(obj, prop, callBack){	
+	var val=obj[prop];	
 	Object.defineProperty(obj, prop, {
 		get: function(){return val;},
 		set: function(v){
@@ -179,7 +171,6 @@ veneer.observe=function(obj, prop, callBack){
 			callBack.call(obj, {type: 'update', name:prop, object: obj, oldValue: v2, value: v });
 		}
 	});		
-
 	return true
 }; // end veneer.observe() 
 
@@ -217,12 +208,12 @@ veneer.k=function k(a){ return a; };
 veneer.date=function(n){return new Date(n||Date.now());};
 veneer.hms=function(s){return new Date(1000*s).toISOString().split("T")[1].split(".")[0];};
 veneer.parseElement=function(elm){
-	
+
 	if(elm._veneered){return}else{elm._veneered=true;}
-	
+
 	var name= elm.getAttribute("name"),
 	 temp=veneer.$('template', elm)[0]||"",
-     def={
+	 def={
 		content: (temp.innerHTML||"").trim(),
 		events:  {},
 		CUSTOM: 1, 
@@ -231,28 +222,28 @@ veneer.parseElement=function(elm){
 		
 		css: ((veneer.$('style', elm)[0]||"").innerHTML||"").trim()
 	};
-	
+
 	elm._frag=temp;	
-	
-   var at=veneer.getAttribs(elm);
-   if(at.proto){
+
+	var at=veneer.getAttribs(elm);
+	if(at.proto){
 	  def.proto=eval(at.proto).prototype;
 	  delete at.proto;
-   }
-   
-  
-   
-   var code=(veneer.$('script[type="shadow"]', elm)[0]||"").innerHTML || "";
-   
-   
-   Object.keys(at).map(function(k){
-     if(/^on/.test(k)) return def.events[k.slice(2)]=Function("e", this[k]);
-     if(k=="name" || k=="id" || k=="proto" || k=="lang" || k=="class" )return;
-     def.props[k]=eval(this[k])||String;
-   }, at);
+	}
 
-   if(temp.attributes && temp.attributes.length){
-	
+
+
+	var code=(veneer.$('script[type="shadow"]', elm)[0]||"").innerHTML || "";
+
+
+	Object.keys(at).map(function(k){
+	 if(/^on/.test(k)) return def.events[k.slice(2)]=Function("e", this[k]);
+	 if(k=="name" || k=="id" || k=="proto" || k=="lang" || k=="class" )return;
+	 def.props[k]=eval(this[k])||String;
+	}, at);
+
+	if(temp.attributes && temp.attributes.length){
+
 	   var tt=veneer.getAttribs(temp);
 	   
 	   Object.keys(tt).map(function(k){
@@ -263,9 +254,8 @@ veneer.parseElement=function(elm){
 		  def.defaults[k]= (def.props[k]||String)(v);
 	   }, tt);
 
-   };//end if template attribs
+	};//end if template attribs
 
-	
 	if(code){
 		if(def.events.insert){
 			var oldIns=def.events.insert, newIns=Function("e", code);
@@ -278,43 +268,27 @@ veneer.parseElement=function(elm){
 			def.events.insert=Function("e", code);	
 		}
 		
-   }
-   
-   
-   return veneer(name, def);
+	}
+	return veneer(name, def);
 };
-
-
-
 
 
 function veneer(tagName, def){
 	
-	def = def || {};
-	
+	def = def || {};	
 	if(!veneer.tags[tagName]){
 			veneer.tags[tagName]=def;
 	}else{
 		return;
 	}
 	
-/*	function curry(fn, arrArgs){
-		if(!arrArgs.join){arrArgs=[arrArgs];}
-		return function _curried(){return fn.apply(this, arrArgs.concat([].slice.call(arguments))) };
-	}
-*/	
-	
 	def.events=def.events||{};
 	
 	var  props=def.props||{},
-	 defs=def.defaults||(def.defaults={}),
-	 methods = def.methods||{},
-	 aPrototype = Object.create( def.proto || HTMLElement.prototype );
+	defs=def.defaults||(def.defaults={}),
+	methods = def.methods||{},
+	aPrototype = Object.create( def.proto || HTMLElement.prototype );
 
-	 if(!props.group){props.group=veneer.k;}
-	// if(!defs.group){defs.group="";}
-
-		
 
 	//this fires on each custom tag at time of creation via markup or createElement():
 	aPrototype.createdCallback = function _init(e){
@@ -322,7 +296,25 @@ function veneer(tagName, def){
 		var that=this; // lets "this" be known as "that" in html event handlers
 
 		that._def=def; 
-
+		var subscribed=that.getAttribute("subscribed")||"";
+		
+		// insert a wrapper around update to push notifications to subscribed elements:
+		if(subscribed){ // && def.events && def.events.update  ){ 
+			var oldUpdate=def.events.update || Boolean;
+			def.events.update=function(){
+				var args=[].slice.call(arguments),
+				 t=this,
+				 resp=oldUpdate.apply(t, args),
+				 ray=veneer.$(subscribed);
+				 that._subscribed=ray;
+				ray.forEach(function(elm){
+					if(elm.change) elm.change(t);
+				});
+				return resp;
+			};
+		
+		}
+		
 		// fire an init event right here
 		if(def.events && def.events.init){
 			def.events.init.call(this, {target: this});
@@ -356,13 +348,13 @@ function veneer(tagName, def){
 	
 
 		// calls the tag definition's update() event ( if defined )
-		function changeme(){ 
-			if(def.events && def.events.update){ def.events.update.call(that, {target: that}); }
+		function changeme(e){ 
+			if(def.events && def.events.update){ def.events.update.call(that, {target: that, e:e}); }
 		};
 
 		if(def.events && def.events.update){ 
 			that.change=changeme; 
-			if( !def.events.insert || def.CUSTOM ){  setTimeout(changeme, 0);  }
+			if( !def.events.insert || def.CUSTOM ){  setTimeout(changeme.bind(that,e), 0);  }
 		}
 		
 
@@ -379,10 +371,6 @@ function veneer(tagName, def){
 	aPrototype.attachedCallback= function _insert(e,b){		
 		setTimeout( veneer.raiseEvent.bind(this, "insert", this), 0);
 	};
-	
-	//aPrototype.attachedCallback=curry(veneer.raiseEvent, "insert" );
-	//aPrototype.detachedCallback=curry(veneer.raiseEvent, "remove" );
-
 	aPrototype.detachedCallback= function _remove(e,b){		
 		setTimeout( veneer.raiseEvent.bind(this, "remove", this), 0);
 	};
@@ -473,4 +461,4 @@ veneer.$("script").map(veneer.set,["loaded", true]);
 
 setTimeout(function(){
 	document.documentElement.className+=" veneer";
-}, 50 );
+}, 30 );
