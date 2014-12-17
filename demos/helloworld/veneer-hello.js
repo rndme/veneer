3be084982fc5f4b2f706b3af441177d1bc2ad0a9
dandavis <rndme@users.noreply.github.com>
2014-12-16 23:07:59 -0600

    repub demos

---
+++ demos/helloworld/veneer-hello.js
@@ -0,0 +1,29 @@
+/**  veneer.js custom html tag definition
+ {
+	tagName: 'veneer-hello',
+	version: '1.0.0',
+	purpose: 'defines a <hello-world> element that says hello to the who attribute value. (for the helloworld.html demo) ',
+	attribs: { who: 'a string name' },
+	events:  [ 'update' ]
+ }
+**/
+
+//////////////////////////////////////////////
+ 
+ veneer("hello-world", {
+	content: "Hello  <strong></strong> :)", 
+	events: {
+		update: function(e){ 
+			var elm=e.target;
+			elm.children[0].innerHTML=elm.who;		
+		}
+	},
+	props:{
+		who: String
+	},
+	defaults:{
+		who: "Anon"
+	}
+	
+  });
+  
--- demos/makered.js
+++
@@ -1,1 +0,0 @@
-document.body.style.color='red';
